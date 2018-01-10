12384b7acad610b66d51ed1f667ddc495282c57c
Kevin Schäfer <K.schaefer92@gmx.de>
2018-01-10 18:59:36 +0100

    Small and Big enemy Char class added

---
+++ Source/Misted_Hope/BigAICharacter.h
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BaseAICharacter.h"
+#include "BigAICharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MISTED_HOPE_API ABigAICharacter : public ABaseAICharacter
+{
+	GENERATED_BODY()
+
+
+public: 
+	ABigAICharacter();
+	
+
+};
